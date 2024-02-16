674e380b493a337c0e0771e7a9a2daf9b713e8f0
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 20:30:40 +0500

    famous Quotes

---
+++ program3/Famous_quotes.py
@@ -0,0 +1,8 @@
+# variable for name
+Prophet_name:str ='Muhammad (P.B.U.H)' 
+# variable for message 
+message : str = f'''
+{Prophet_name} says,"Seek knowledge from infancy to the grave"
+'''
+#Printing message
+print(message)
